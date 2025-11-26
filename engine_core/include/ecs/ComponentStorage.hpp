7c4dbbc548b2dae0b21f204665c201f9a056a468
Erwan <erwan.itier@epitech.eu>
2025-11-26 16:05:08 +0100

    [ADD] archi for ecs

---
+++ engine_core/include/ecs/ComponentStorage.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** R-type-mirror
+** File description:
+** ComponentStorage.hpp
+*/
+
+#include "ecs/Entity.hpp"
+#include <unordered_map>
+
+#ifndef ECS_COMPONENTSTORAGE_HPP_
+
+template <typename T> class ComponentStorage
+{
+  public:
+    void addComponent();
+
+  private:
+};
+
+#endif
---
+++ engine_core/include/ecs/Entity.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** R-type-mirror
+** File description:
+** Entity.hpp
+*/
+
+#include <cstdint>
+
+#ifndef ECS_ENTITY_HPP_
+
+using Entity = std::uint32_t;
+
+#endif
