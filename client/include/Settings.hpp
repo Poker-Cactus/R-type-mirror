772d3c13141e51962bdae0254fffbc3266d2b78b
NeauNeauu <noah.ney@epitech.eu>
2026-01-14 18:09:49 +0100

    add framerate

--- client/include/Settings.hpp
+++ client/include/Settings.hpp
@@ -50,7 +50,7 @@ public:
 
   // GRAPHIC Settings
   bool fullScreen = true; ///< Fullscreen mode toggle
-  int targetFPS = 60; ///< Target framerate limit (60-240)
+  int targetFPS = 30; ///< Target framerate limit (60-240)
 
   // DEBUG Settings
   bool showInfoMode = true; ///< Show debug info overlay
